925ab7493c69ed4d054515cff25f5b9d60bedb37
Shivam <loyalgamer87@gmail.com>
2025-04-11 16:33:29 +0530

    changes in routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,17 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import ShowDetailPage from "./Pages/ShowDetails.Page";
 import ShowListPage from "./Pages/ShowsList.Page";
 
 function App() {
   return (
-    <BrowserRouter>
     <div className="min-h-screen bg-gradient-to-br from-blue-300 to-purple-400 bg-cover bg-fixed bg-center">
-      <div className="max-w-5xl mx-auto  min-h-screen mt pt-2">
-          <Routes>
-            <Route path="/" element={<ShowListPage />} />
-            <Route path="show/:showId" element={<ShowDetailPage />} />
-          </Routes>
+      <div className="max-w-5xl mx-auto min-h-screen pt-2">
+        <Routes>
+          <Route path="/" element={<ShowListPage />} />
+          <Route path="show/:showId" element={<ShowDetailPage />} />
+        </Routes>
       </div>
     </div>
-    </BrowserRouter>
   );
 }
 
